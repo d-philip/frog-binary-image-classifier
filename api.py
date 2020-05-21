f3a6d597dae314a05b6423881fada85886c1336f
d-philip <djphilip@bu.edu>
2020-05-20 23:41:43 -0400

    Changed classification API to grab correct data.

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from model import CNNModel
 
 app = Flask(__name__)
@@ -7,10 +7,16 @@ model.deserialize('final_model.h5')
 
 @app.route('/predict/', methods=['POST'])
 def predict_class():
-    if request.method== 'POST':
-        print (request.files['file'])
-        if request.files:
-            image = request.files['file']
-            pred_dict = model.predict(image)
-            return pred_dict
-        abort(404)
+    if request.method == 'POST':
+        try:
+            image = request.get_data(as_text=True)
+            image_dict = loads(image)
+            img_pix = model.load_image(image_dict['image'], 1)
+            pred_dict = model.predict(img_pix)
+            return(pred_dict)
+        except:
+            error = {'error': 'Error with server.'}
+            return(error)
+    else:
+        error = {'error': 'Request method is invalid.'}
+        return(error)
